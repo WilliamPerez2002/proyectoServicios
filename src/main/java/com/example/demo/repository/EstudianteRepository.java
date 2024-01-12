19305aa23d818ddd6a96fbe3de43107c8a425dd3
WilliamPerez2002 <129987049+WilliamPerez2002@users.noreply.github.com>
2024-01-12 02:14:38 -0500

    Revert "Feature/token"

--- src/main/java/com/example/demo/repository/EstudianteRepository.java
+++ src/main/java/com/example/demo/repository/EstudianteRepository.java
@@ -2,9 +2,6 @@ package com.example.demo.repository;
 import com.example.demo.model.Estudiante;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EstudianteRepository extends JpaRepository<Estudiante,String> {
 
-    List<Estudiante> findByCedulaContaining(String cedula);
 }
